a07397ab54081775e45b0998ca0789423480af90
Mattia Righetti <mattia.righetti@nexi.it>
2023-11-16 21:26:40 +0100

    Prima implementazione Pay-By-Link

--- index.php
+++ index.php
@@ -1,3 +1,89 @@
+<?php
+// Se ho l'amount in POST vuol dire che ho richiesto generazione del Pay-By-Link, quindi procedo
+if ($_POST["amount"]) {
+        
+    // Richiesta link PayMail
+
+    $connection = curl_init();
+
+    if ($connection) {
+
+        $requestURL = "https://int-ecommerce.nexi.it/"; // URL
+        $requestURI = "ecomm/api/bo/richiestaPayMail"; // URI
+        
+        // Parametri per calcolo MAC
+        $apiKey = "ALIAS_WEB_00073202"; // Alias fornito da Nexi
+        $chiaveSegreta = "JW7O76VCXA01QF1GUIUBNNSTI2IZSK2S"; // Chiave segreta fornita da Nexi
+        $codiceTransazione = "APIBO_" . date('YmdHis'); // Codice della transazione
+        $importo = $_POST["amount"]; // 5000 = 50,00 EURO (indicare la cifra in centesimi)
+        $timeout = 4; // Durata in ore del link di pagamento che verrà generato 
+        $url = "https://" . filter_input(INPUT_SERVER, 'HTTP_HOST') . "/esito.php"; // URL dove viene rimandato il cliente al termine del pagamento (prefisso necessario http:// oppure https://)
+        $urlBack = "https://" . filter_input(INPUT_SERVER, 'HTTP_HOST') . "/back.php"; // URL dove viene rimandato il cliente in caso di annullamento del pagamento (prefisso necessario http:// oppure https://)
+        $urlPost = "https://" . filter_input(INPUT_SERVER, 'HTTP_HOST') . "/notifica.php"; // URL verso il quale viene fatta la notifica del pagamento (prefisso necessario http:// oppure https://)
+        $timeStamp = (time()) * 1000;
+
+        // Calcolo MAC
+        $mac = sha1("apiKey=" . $apiKey . "codiceTransazione=" . $codiceTransazione . "importo=" . $importo . "timeStamp=" . $timeStamp . $chiaveSegreta);
+
+        // Parametri
+        $parametri = array(
+            // Obbligatori
+            'apiKey' => $apiKey,
+            'importo' => $importo,
+            'codiceTransazione' => $codiceTransazione,
+            'timeStamp' => $timeStamp,
+            'mac' => $mac
+        );
+
+        // Controllo se ho i parametri aggiuntivi opzionali e nel caso li aggiungo
+        $parametriAggiuntivi;
+        if (!empty($_POST['mail'])) {
+            array_push($parametriAggiuntivi, array('mail' => $_POST['mail']));
+        }
+        if (!empty($_POST['nome'])) {
+            array_push($parametriAggiuntivi, array('nome' => $_POST['nome']));
+        }
+        if (!empty($_POST['cognome'])) {
+            array_push($parametriAggiuntivi, array('cognome' => $_POST['cognome']));
+        }
+        if (!empty($_POST['descrizione'])) {
+            array_push($parametriAggiuntivi, array('descrizione' => $_POST['descrizione']));
+        }
+        if (!empty($parametriAggiuntivi)) {
+            array_push($parametri, array('parametriAggiuntivi' => $parametriAggiuntivi))
+        }
+
+        curl_setopt_array($connection, array(
+            CURLOPT_URL => $requestURL . $requestURI,
+            CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => json_encode($parametri),
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLINFO_HEADER_OUT => true,
+            CURLOPT_SSL_VERIFYPEER => 0
+        ));
+
+        $json = curl_exec($connection);
+
+        curl_close($connection);
+
+        // Decodifico risposta
+        $risposta = json_decode($json, true);
+
+        // Controllo JSON di risposta
+        if (json_last_error() === JSON_ERROR_NONE) {
+
+            $MACrisposta = sha1('esito=' . $risposta['esito'] . 'idOperazione=' . $risposta['idOperazione'] . 'timeStamp=' . $risposta['timeStamp'] . $chiaveSegreta);
+
+        } else {
+            echo 'Errore nella lettura del JSON di risposta';
+        }
+    } else {
+        echo "Errore curl";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -27,7 +113,7 @@
             <div id="new-order" class="page active">
                 <h1>New order</h1>
 
-                <form action="" method="post">
+                <form action="index.php" method="post">
                     <div class="order-customer">
                         <div>
                             <h3 class="sub-title">Customer</h3>
@@ -132,6 +218,27 @@
 
                     <input type="submit" class="submit-btn" value="Generate payment link" />
                 </form>
+
+                <?php
+                // Se ho l'amount in POST vuol dire che ho richiesto generazione del Pay-By-Link quindi ritorno l'esito
+                if ($_POST["amount"]) {
+                    // Controllo MAC di risposta
+                    if ($risposta['mac'] == $MACrisposta) {
+
+                        // Controllo esito
+                        if ($risposta['esito'] == 'OK') {
+                            echo 'Operazione n. ' . $risposta['idOperazione'] . ' eseguita<br>';
+                            echo "Link generato correttamente: " . $risposta['payMailUrl'] . "<br>";
+                            echo "<a href ='" . $risposta['payMailUrl'] . "'>VAI AL LINK</a>";
+                        } else {
+                            echo 'Operazione n. ' . $risposta['idOperazione'] . ' non eseguita. esito ' . $risposta['esito'] . '<br><br>' . json_encode($risposta['errore']);
+                        }
+                    } else {
+                        echo 'Errore nel calcolo del MAC di risposta';
+                    }               
+                }
+                ?>
+
             </div>
             <!---->
 
